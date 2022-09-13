f09f2a9719d9056ea8c8cf349901ef21b5563c0b
lyran10 <liranramekar629@gmail.com>
2022-09-13 13:00:02 +0300

    chat-app

--- backend/data/dummyData.js
+++ backend/data/dummyData.js
@@ -105,29 +105,6 @@ const chats = [
       email: "guest@example.com",
     },
   },
-  {
-    isGroupChat: true,
-    users: [
-      {
-        name: "John Doe",
-        email: "jon@example.com",
-      },
-      {
-        name: "Piyush",
-        email: "piyush@example.com",
-      },
-      {
-        name: "Guest User",
-        email: "guest@example.com",
-      },
-    ],
-    _id: "617a518c4081150016472c78",
-    chatName: "Chill Zone",
-    groupAdmin: {
-      name: "Guest User",
-      email: "guest@example.com",
-    },
-  },
 ];
 
 module.exports = {chats}
